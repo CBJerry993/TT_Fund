ac904b2f1bad7c62d77f8d3a7aaa021da6675616
CBJerry <CBJerry@qq.com>
2021-04-23 16:47:18 +0800

    修改日期为2021年和2020年，19年的弱需要请自行修改此行。

--- tt_fund/spiders/fund_earning.py
+++ tt_fund/spiders/fund_earning.py
@@ -80,7 +80,7 @@ class FundEarningSpider(scrapy.Spider):
                 )
             # 2.3基金10大持仓股(指定按年)
             if self.need_fund_position:
-                for year in ["2019", "2020"]:
+                for year in ["2020", "2021"]:
                     yield scrapy.Request(
                         "http://fundf10.eastmoney.com/FundArchivesDatas.aspx?type=jjcc&code={}&topline=10&year={}"
                         "&month=".format(item["code"].strip(), year),
